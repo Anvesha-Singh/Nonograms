c84f9c5a5fdc0c9dbc97f263842240c7777d7bad
Dhvani Thakkar <dhvani.r.thakkar@gmail.com>
2023-05-31 20:11:43 +0530

    Minor error in calling a function

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def solve_nonogram(all_clues: list[list[str]]) -> list[list[str]]:
 
         nonogram_grid = fill_complete_with_crosses(nonogram_grid, all_clues[0])
         transpose =  make_list_of_lists(list(zip(*nonogram_grid)))
-        transpose = fill_complete_with_crosses(nonogram_grid, all_clues[1])
+        transpose = fill_complete_with_crosses(transpose, all_clues[1])
         nonogram_grid = make_list_of_lists(list(zip(*transpose)))
 
         #nonogram_grid = fill_for_bigger_than_half(nonogram_grid, all_clues[0])
