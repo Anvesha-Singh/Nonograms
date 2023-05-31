20f4fe8f028e23830ed7b1a5b3d944c519505302
Dhvani Thakkar <dhvani.r.thakkar@gmail.com>
2023-05-31 12:47:52 +0530

    I made a file that will contain functions for number inputs of different patterns. Each function called will return a list with two elements: the list input for the rows, and the list input for the columns. The list input for the rows/columns will have string elements, with "3 1" format and a space will seperate the two numbers input for that particular row/column

---
+++ numbers_input.py
@@ -0,0 +1,4 @@
+def cross_pattern() -> list[list[str]]:
+	row_numbers_input = ["3", "2 2", "1 1", "2 2", "3"]
+	column_numbers_input = ["3", "2 2", "1 1", "2 2", "3"]
+	return [row_numbers_input, column_numbers_input]
