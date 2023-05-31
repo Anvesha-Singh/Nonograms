f6c505c6e6b70c8438a5aa9fc14b8979dea1f975
Dhvani Thakkar <dhvani.r.thakkar@gmail.com>
2023-05-31 20:36:57 +0530

    Called function to add crosses to a fully filled line

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def solve_nonogram(all_clues: list[list[str]]) -> list[list[str]]:
         transpose = fill_for_complete_rows(transpose, all_clues[1])        
         nonogram_grid = make_list_of_lists(list(zip(*transpose)))
 
+	nonogram_grid = add_crosses_to_full_rows(nonogram_grid, all_clues[0])
+        transpose =  make_list_of_lists(list(zip(*nonogram_grid)))
+        transpose = add_crosses_to_full_rows(transpose, all_clues[1])
+        nonogram_grid = make_list_of_lists(list(zip(*transpose)))
 
         nonogram_grid = fill_complete_with_crosses(nonogram_grid, all_clues[0])
         transpose =  make_list_of_lists(list(zip(*nonogram_grid)))
