53da99221e58c47861fb2be6bb9c03427d1523c4
Dhvani Thakkar <dhvani.r.thakkar@gmail.com>
2023-05-31 12:11:14 +0530

    I created a basic documentation template using Sphinx.

---
+++ Nonogram_Documentation/docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Nonogram Bot'
+copyright = '2023, Anvesha Dhvani Jyothika Sanya'
+author = 'Anvesha Dhvani Jyothika Sanya'
+release = '2023'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ Nonogram_Documentation/docs/source/index.rst
@@ -0,0 +1,16 @@
+.. Nonogram Bot documentation master file, created by
+   sphinx-quickstart on Wed May 31 12:00:52 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to the Nonogram Bot!
+========================================
+
+
+Navigation
+--------------------
+.. toctree::
+   :maxdepth: 2
+   Game Rules<gamerules>
+   Features<features>
+   Our Code<code>
